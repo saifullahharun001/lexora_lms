79dffafc9bf1952172faa82a4cef390071fbbfcc
Saifullah Harun <saifullahharun001@gmail.com>
2026-05-21 07:08:03 +0000

    Fix notice notification module dependency

--- apps/api/src/modules/notice/notice.module.ts
+++ apps/api/src/modules/notice/notice.module.ts
@@ -3,6 +3,7 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "@/common/prisma/prisma.module";
 import { RequestContextModule } from "@/common/request-context/request-context.module";
 import { AuthorizationModule } from "@/modules/authorization/authorization.module";
+import { NotificationModule } from "@/modules/notification/notification.module";
 import { PlatformModule } from "@/platform/platform.module";
 import { NoticeService } from "./application/services/notice.service";
 import { NOTICE_REPOSITORY } from "./domain/notice.constants";
@@ -13,6 +14,7 @@ import { NoticesController } from "./presentation/http/notices.controller";
   imports: [
     PlatformModule,
     AuthorizationModule,
+    NotificationModule,
     PrismaModule,
     RequestContextModule
   ],
